c7aba349cc38e600102d7ea577d58b5e9efb55c1
Steven <stevensilalahi75@gamil.com>
2025-04-20 00:23:54 +0700

    Temporary navigation

---
+++ nav/nav.php
@@ -0,0 +1,10 @@
+<?php
+echo "<nav>
+        <ul>
+            <li><a href='../profile/profile.php'>Profile</a></li>
+            <li><a href='../regular/regular_admin.html'>Regular</a></li>
+            <li><a href='../regular/track.php'>Track</a></li>
+        </ul>
+        </nav>
+    ";
+?>
