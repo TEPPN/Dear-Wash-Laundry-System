65fa04432b429555e332ec4c0bdf682e014ac814
Steven <stevensilalahi75@gamil.com>
2025-04-24 11:14:05 +0700

    adding logout

--- profile/profile.php
+++ profile/profile.php
@@ -5,6 +5,7 @@
     $user_name = $_SESSION['user_name'];
     $user_id = $_SESSION['user_id'];
 
+    echo '<a href="../login/logout.php"><button> Log Out </button></a>';
     echo "<h1>Welcome, $user_name</h1>";
     echo "<h2>Your Profile</h2>";
     echo "<p>User ID: $user_id</p>";    
